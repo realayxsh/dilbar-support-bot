491291339ffefeb74bcba1af5cb61656dbb95083
realayxsh <realayxsh@gmail.com>
2026-06-25 11:41:13 +0530

    fix: antikick.py

--- bot/cogs/events/antikick.py
+++ bot/cogs/events/antikick.py
@@ -30,7 +30,7 @@ proxies={"http": 'http://' + next(proxs)}
 class antikick(Cog):
     def __init__(self, client: Ventura):
         self.client = client      
-        self.headers = {"Authorization": ff"Bot {os.environ.get('TOKEN', '')}"}
+        self.headers = {"Authorization": f"Bot {os.environ.get('TOKEN', '')}"}
 
 
         
